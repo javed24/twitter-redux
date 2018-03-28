99bd5bef75546843767474fa5b2a91172eeb5f0a
javed24 <aquib_javed@live.com>
2018-03-27 21:02:06 -0400

    getting app data

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,6 +1,6 @@
-import {getInitialData} from '../utis/api';
+import {getInitialData} from '../utils/api';
 import { receiveUsers } from '../actions/users';
-import { receivePolls } from '../actions/polls';
+import { receiveTweets } from '../actions/tweets';
 import { setAuthedUser } from '../actions/authedUser';
 
 const AUTHED_ID = 'tylermcginnis';
